ea4eb55c032dc2afa08dcf6b6d9e0de288c9ace1
pnghfng <pnghfng52388@icloud.com>
2025-10-25 17:26:57 +0800

    保存可用版本，待优化

--- src/test/mesh.hpp
+++ src/test/mesh.hpp
@@ -16,42 +16,9 @@ class vMesh
 {
   public:
     Vertexs _vertices;
-    std::vector<unsigned int> _indices;
+    std::vector<GLuint> _indices;
     std::vector<Texture> _textures;
-    vMesh(Vertexs vertices, std::vector<unsigned int> indices, std::vector<Texture> textures)
-        : _vertices{vertices}, _indices{indices}, _textures{textures}
-    {
-        setupMesh();
-    }
     vMesh(aiMesh* mesh, const aiScene* scene, std::string_view directory)
-    {
-        setup_va(mesh, scene);
-        if (mesh->mMaterialIndex >= 0)
-        {
-            TEXTURE_MANAGER.load_from_material(scene->mMaterials[mesh->mMaterialIndex], _textures, directory);
-        }
-    }
-    void Draw(ShaderProgram& shader) const
-    {
-        shader.use();
-        int sampler_offset = shader.get_samplers_ct();
-        for (int i = 0; i < _textures.size(); i++)
-        {
-            shader.set_uniform<int>(_textures[i].type, sampler_offset);
-            glActiveTexture(GL_TEXTURE0 + sampler_offset);
-            glBindTexture(GL_TEXTURE_2D, _textures[i].id);
-            sampler_offset++;
-        }
-        // glBindVertexArray(vao);
-        vva.bind();
-        glDrawElements(GL_TRIANGLES, _indices.size(), GL_UNSIGNED_INT, 0);
-        vva.unbind();
-    }
-    vVertexArray vva;
-    size_t _ct;
-    GLuint vao;
-  private:
-    void setup_va(aiMesh* mesh, const aiScene* scene)
     {
         std::vector<Vertex> vertices;
         std::vector<unsigned int> indices;
@@ -77,29 +44,31 @@ class vMesh
         GLuint vb = BUFFER.generate_buffer(GL_ELEMENT_ARRAY_BUFFER, vertices.size() * sizeof(float) * 8, vertices.data());
         GLuint eb = BUFFER.generate_buffer(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data());
         vva.attach_buffer(PNT_LAYOUT, vb, eb);
+        if (mesh->mMaterialIndex >= 0)
+        {
+            TEXTURE_MANAGER.load_from_material(scene->mMaterials[mesh->mMaterialIndex], _textures, directory);
+        }
+        _ct = indices.size();
     }
-
-    void setupMesh()
+    void Draw(ShaderProgram& shader) const
     {
-
-        GLuint vb = BUFFER.generate_buffer(GL_ELEMENT_ARRAY_BUFFER, _vertices.size() * sizeof(float) * 8, _vertices.data());
-        GLuint eb = BUFFER.generate_buffer(GL_ELEMENT_ARRAY_BUFFER, _indices.size() * sizeof(unsigned int), _indices.data());
-        vva.attach_buffer(PNT_LAYOUT, vb, eb);
-        
-        // glGenVertexArrays(1, &vao);
-        // // glBindVertexArray(vao);
-        // vva.bind();
-
-        // glBindBuffer(GL_ARRAY_BUFFER, vb);
-        // glEnableVertexAttribArray(0);	
-        // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 32, (void*)0);
-        // glEnableVertexAttribArray(1);	
-        // glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 32, (void*)12);
-        // glEnableVertexAttribArray(2);	
-        // glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 32, (void*)24);
-        // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eb);
-        // glBindVertexArray(0);
+        shader.use();
+        int sampler_offset = shader.get_samplers_ct();
+        for (int i = 0; i < _textures.size(); i++)
+        {
+            shader.set_uniform<int>(_textures[i].type, sampler_offset);
+            glActiveTexture(GL_TEXTURE0 + sampler_offset);
+            glBindTexture(GL_TEXTURE_2D, _textures[i].id);
+            sampler_offset++;
+        }
+        // glBindVertexArray(vao);
+        vva.bind();
+        glDrawElements(GL_TRIANGLES, fmax(_ct, _indices.size()), GL_UNSIGNED_INT, 0);
+        vva.unbind();
     }
+    vVertexArray vva;
+    size_t _ct;
+    GLuint vao;
 };
 
 class vModel
@@ -145,8 +114,8 @@ class vModel
         for (unsigned int i = 0; i < node->mNumMeshes; i++)
         {
             aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
-            _meshes.push_back(processMesh(mesh, scene));
-            // _meshes.emplace_back(mesh, scene, _directory);
+            // _meshes.push_back(processMesh(mesh, scene));
+            _meshes.emplace_back(mesh, scene, _directory);
         }
         // 接下来对它的子节点重复这一过程
         for (unsigned int i = 0; i < node->mNumChildren; i++)
