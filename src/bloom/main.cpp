8e929b9dd9ce6145396fe854724c18b1225e7f8f
pnghfng <pnghfng52388@icloud.com>
2025-10-31 17:19:02 +0800

    pass1完成

--- src/bloom/main.cpp
+++ src/bloom/main.cpp
@@ -18,17 +18,17 @@ public:
 
 std::vector<glm::vec3> lightPositions
 {
-    glm::vec3(0.0f, 0.0f, 49.5f),
-    glm::vec3(-1.4f, -1.9f, 9.0f),
-    glm::vec3(0.0f, -1.8f, 4.0f),
-    glm::vec3(0.8f, -1.7f, 6.0f),
+   glm::vec3( 0.0f, 0.5f,  1.5f),
+   glm::vec3(-4.0f, 0.5f, -3.0f),
+   glm::vec3( 3.0f, 0.5f,  1.0f),
+   glm::vec3(-.8f,  2.4f, -1.0f),
 };
 std::vector<glm::vec3> lightColors
 {
-    glm::vec3(200.0f, 200.0f, 200.0f),
-    glm::vec3(0.1f, 0.0f, 0.0f),
-    glm::vec3(0.0f, 0.0f, 0.2f),
-    glm::vec3(0.0f, 0.1f, 0.0f),
+    glm::vec3(5.0f, 5.0f, 5.0f),
+    glm::vec3(10.0f, 0.0f, 0.0f),
+    glm::vec3(0.0f, 0.0f, 15.0f),
+    glm::vec3(0.0f, 5.0f, 0.0f),
 };
 
 class BloomWidget : public GLWidget
